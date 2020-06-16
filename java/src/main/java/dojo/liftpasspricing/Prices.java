e45e005a978de2e5e943967e2f930261758884d1
Gautier DI FOLCO <gdifolco@linagora.com>
2020-06-16 15:29:51 +0200

    Coding dojo 2020-06-16

--- java/src/main/java/dojo/liftpasspricing/Prices.java
+++ java/src/main/java/dojo/liftpasspricing/Prices.java
@@ -11,9 +11,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
+import spark.Request;
 
 public class Prices {
 
@@ -39,92 +42,101 @@ public class Prices {
             return "";
         });
 
-        get("/prices", (req, res) -> {
-            final Integer age = req.queryParams("age") != null ? Integer.valueOf(req.queryParams("age")) : null;
-
-            try (PreparedStatement costStmt = connection.prepareStatement( //
-                    "SELECT cost FROM base_price " + //
-                    "WHERE type = ?")) {
-                costStmt.setString(1, req.queryParams("type"));
-                try (ResultSet result = costStmt.executeQuery()) {
-                    result.next();
-
-                    int reduction;
-                    boolean isHoliday = false;
-
-                    if (age != null && age < 6) {
-                        return "{ \"cost\": 0}";
-                    } else {
-                        reduction = 0;
-
-                        if (!req.queryParams("type").equals("night")) {
-                            DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-                            try (PreparedStatement holidayStmt = connection.prepareStatement( //
-                                    "SELECT * FROM holidays")) {
-                                try (ResultSet holidays = holidayStmt.executeQuery()) {
-
-                                    while (holidays.next()) {
-                                        Date holiday = holidays.getDate("holiday");
-                                        if (req.queryParams("date") != null) {
-                                            Date d = isoFormat.parse(req.queryParams("date"));
-                                            if (d.getYear() == holiday.getYear() && //
-                                                d.getMonth() == holiday.getMonth() && //
-                                                d.getDate() == holiday.getDate()) {
-                                                isHoliday = true;
-                                            }
-                                        }
-                                    }
-
-                                }
-                            }
-
-                            if (req.queryParams("date") != null) {
-                                Calendar calendar = Calendar.getInstance();
-                                calendar.setTime(isoFormat.parse(req.queryParams("date")));
-                                if (!isHoliday && calendar.get(Calendar.DAY_OF_WEEK) == 2) {
-                                    reduction = 35;
-                                }
-                            }
-
-                            // TODO apply reduction for others
-                            if (age != null && age < 15) {
-                                return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .7) + "}";
-                            } else {
-                                if (age == null) {
-                                    double cost = result.getInt("cost") * (1 - reduction / 100.0);
-                                    return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
-                                } else {
-                                    if (age > 64) {
-                                        double cost = result.getInt("cost") * .75 * (1 - reduction / 100.0);
-                                        return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
-                                    } else {
-                                        double cost = result.getInt("cost") * (1 - reduction / 100.0);
-                                        return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
-                                    }
-                                }
-                            }
-                        } else {
-                            if (age != null && age >= 6) {
-                                if (age > 64) {
-                                    return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .4) + "}";
-                                } else {
-                                    return "{ \"cost\": " + result.getInt("cost") + "}";
-                                }
-                            } else {
-                                return "{ \"cost\": 0}";
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        get("/prices", (req, res) -> getPrice(connection, req));
 
         after((req, res) -> {
             res.type("application/json");
         });
 
         return connection;
+    }
+
+    private static String getPrice(Connection connection, Request req) throws SQLException, ParseException {
+        String requestAge = req.queryParams("age");
+        String requestType = req.queryParams("type");
+        String requestDate = req.queryParams("date");
+        return getPrice(connection, requestAge, requestType, requestDate);
+    }
+
+    public static String getPrice(Connection connection, String requestAge, String requestType, String requestDate) throws SQLException, ParseException {
+        final Integer age = requestAge != null ? Integer.valueOf(requestAge) : null;
+
+        try (PreparedStatement costStmt = connection.prepareStatement( //
+                "SELECT cost FROM base_price " + //
+                "WHERE type = ?")) {
+            costStmt.setString(1, requestType);
+            try (ResultSet result = costStmt.executeQuery()) {
+                result.next();
+
+                int reduction;
+                boolean isHoliday = false;
+
+                if (age != null && age < 6) {
+                    return "{ \"cost\": 0}";
+                } else {
+                    reduction = 0;
+
+                    if (!requestType.equals("night")) {
+                        DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+                        try (PreparedStatement holidayStmt = connection.prepareStatement( //
+                                "SELECT * FROM holidays")) {
+                            try (ResultSet holidays = holidayStmt.executeQuery()) {
+
+                                while (holidays.next()) {
+                                    Date holiday = holidays.getDate("holiday");
+                                    if (requestDate != null) {
+                                        Date d = isoFormat.parse(requestDate);
+                                        if (d.getYear() == holiday.getYear() && //
+                                            d.getMonth() == holiday.getMonth() && //
+                                            d.getDate() == holiday.getDate()) {
+                                            isHoliday = true;
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+
+                        if (requestDate != null) {
+                            Calendar calendar = Calendar.getInstance();
+                            calendar.setTime(isoFormat.parse(requestDate));
+                            if (!isHoliday && calendar.get(Calendar.DAY_OF_WEEK) == 2) {
+                                reduction = 35;
+                            }
+                        }
+
+                        // TODO apply reduction for others
+                        if (age != null && age < 15) {
+                            return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .7) + "}";
+                        } else {
+                            if (age == null) {
+                                double cost = result.getInt("cost") * (1 - reduction / 100.0);
+                                return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
+                            } else {
+                                if (age > 64) {
+                                    double cost = result.getInt("cost") * .75 * (1 - reduction / 100.0);
+                                    return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
+                                } else {
+                                    double cost = result.getInt("cost") * (1 - reduction / 100.0);
+                                    return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
+                                }
+                            }
+                        }
+                    } else {
+                        if (age != null && age >= 6) {
+                            if (age > 64) {
+                                return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .4) + "}";
+                            } else {
+                                return "{ \"cost\": " + result.getInt("cost") + "}";
+                            }
+                        } else {
+                            return "{ \"cost\": 0}";
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -2,12 +2,34 @@ package dojo.liftpasspricing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
@@ -19,31 +41,75 @@ public class PricesTest {
 
     @BeforeEach
     public void createPrices() throws SQLException {
-        connection = Prices.createApp();
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lift_pass", "root", "mysql");
+//        connection = Prices.createApp();
     }
 
     @AfterEach
     public void stopApplication() throws SQLException {
-        Spark.stop();
+//        Spark.stop();
         connection.close();
     }
 
-    @Test
-    public void doesSomething() {
-        JsonPath response = RestAssured.
-            given().
-                port(4567).
-            when().
-                // construct some proper url parameters
-                get("/prices").
-            then().
-                assertThat().
-                    statusCode(200).
-                assertThat().
-                    contentType("application/json").
-            extract().jsonPath();
+//    @Test
+//    public void doesSomething() {
+//        JsonPath response = RestAssured.
+//            given().
+//                port(4567).
+//            when().
+//                // construct some proper url parameters
+//                get("/prices?type=night&age=23&date=2019-02-18").
+//
+//            then().
+//                assertThat().
+//                    statusCode(200).
+//                assertThat().
+//                    contentType("application/json").
+//            extract().jsonPath();
+//
+//        assertEquals(Integer.valueOf(19), response.get("cost"));
+//    }
 
-        assertEquals("putSomehtingHere", response.get("putSomehtingHere"));
+    @ParameterizedTest
+    @MethodSource
+    public void testGetPrice(String requestAge, String requestType, String requestDate, String output) throws Exception {
+        assertEquals(output, Prices.getPrice(connection, requestAge, requestType, requestDate));
     }
 
+    private static Stream<Arguments> testGetPrice() {
+        return Stream.of(
+                Arguments.of(null, "night", "2019-02-18", "{ \"cost\": 0}"),
+                Arguments.of("23", "night", "2019-02-18", "{ \"cost\": 19}"),
+                Arguments.of("23", "1jour", "2019-02-18", "{ \"cost\": 35}")
+        );
+    }
+
+    @Test
+    public void testGetPriceGolden() throws Exception {
+        List<String> ages = IntStream.range(1, 76).mapToObj(Integer::toString).collect(Collectors.toList());
+        ages.add(null);
+        List<String> types = Arrays.asList("night", "1jour");
+        List<String> dates = LongStream.range(0, 365).mapToObj(increment ->
+                LocalDate.of(2019, 1, 1)
+                        .plusDays(increment)
+                        .format(DateTimeFormatter.ofPattern("YYYY-MM-DD")))
+                .collect(Collectors.toList());
+
+        List<String> outputs = new ArrayList<>();
+        for (String age : ages) {
+            for (String date : dates) {
+                for (String type : types) {
+                    outputs.add(Prices.getPrice(connection, age, type, date));
+                }
+            }
+        }
+        FileWriter fileWriter = new FileWriter("ranTest.txt");
+        for (String output : outputs) {
+            fileWriter.write(output + "\n");
+        }
+        fileWriter.close();
+        List<String> expected = Files.lines(Paths.get("goldenMaster.txt")).collect(Collectors.toList());
+
+        assertEquals(expected, outputs);
+    }
 }
